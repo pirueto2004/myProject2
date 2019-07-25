d5a5910082cd917b9ef5d00499b8a181fa70ee79
Julio Bell <juliobell2014@gmail.com>
2019-07-24 22:44:00 -0400

    Updated passport.js

--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -18,43 +18,10 @@ module.exports = function(app) {
     });
   });
 
-    //   app.get("/orders/:id", function(req, res) {
-    //     // Here we add an "include" property to our options in our findOne query
-    //     // We set the value to an array of the models we want to include in a left outer join
-    //     // In this case, just db.Post
-    //     db.Order.findOne({
-    //       where: {
-    //         OrderId: req.params.id
-    //       }
-    //       //include: [db.Product]
-    //     }).then(function(dbOrder) {
-    //       res.json(dbOrder);
-    //     });
-    //   });
-
-    //   app.post("/users", function(req, res) {
-    //     db.User.create({
-    //       id: req.body.id // mySQL:jQuery
-    //     }).then(function(dbUser) {
-    //       res.json(dbUser);
-    //     });
-    //   });
-
-  // New Product route for saving a new product
+  // New User route for saving a new user
   app.post("/users", function(req, res) {
     db.User.create(req.body).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
-
-  //   app.delete("/orders/:id", function(req, res) {
-  //     // console.log(typeof req.params.id);
-  //     db.Order.destroy({
-  //       where: {
-  //         OrderId: req.params.id
-  //       }
-  //     }).then(function(dbOrder) {
-  //       res.json(dbOrder);
-  //     });
-  //   });
 };
